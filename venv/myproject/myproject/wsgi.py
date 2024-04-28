29ada3a36860815485b6f7ba93bd4809c352e47b
LynnAkinyi <78434875+LynnAkinyi@users.noreply.github.com>
2024-04-29 01:13:53 +0300

    commit

--- venv/myproject/myproject/wsgi.py
+++ venv/myproject/myproject/wsgi.py
@@ -12,15 +12,15 @@ import sys
 from django.core.wsgi import get_wsgi_application
 
 # Add your project directory to the Python path
-# project_path = 'C:\\Users\\pc\\OneDrive\\Desktop\\AUTH_SYSTEM\\venv\\myproject\\myproject'
-# if os.path.exists(project_path) and os.path.isfile(os.path.join(project_path, '__init__.py')):
-#     sys.path.insert(0, project_path)
-# else:
-#     print(f"Project path: {project_path} does not exist or is not a valid Python package.")
+project_path = 'C:\\Users\\pc\\OneDrive\\Desktop\\AUTH_SYSTEM\\venv\\myproject\\myproject'
+if os.path.exists(project_path) and os.path.isfile(os.path.join(project_path, '__init__.py')):
+    sys.path.insert(0, project_path)
+else:
+    print(f"Project path: {project_path} does not exist or is not a valid Python package.")
 
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
 application = get_wsgi_application()
-# app = application
+app = application
