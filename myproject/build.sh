a3f7c7f4377e14e2377858ca4fe78d680c43ebcb
LynnAkinyi <lynnakinyi2003@gmail.com>
2025-05-01 00:43:49 +0300

    installed modules

--- myproject/build.sh
+++ myproject/build.sh
@@ -4,8 +4,8 @@ set -e
 # Install dependencies
 pip install -r requirements.txt
 
-# Set correct Django settings module
-export DJANGO_SETTINGS_MODULE=transaction_site.settings
+# Set correct Django settings module 
+export DJANGO_SETTINGS_MODULE=myproject.settings  # Change to your actual project name
 
 # Run Django commands
 python manage.py collectstatic --noinput
