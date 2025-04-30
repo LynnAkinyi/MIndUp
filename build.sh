b0c29c1b021bb89b425550d3d124d0a9c0222b9c
LynnAkinyi <lynnakinyi2003@gmail.com>
2025-05-01 00:47:27 +0300

    changed location

--- build.sh
+++ build.sh
@@ -3,10 +3,9 @@ set -e
 
 # Install dependencies
 pip install -r requirements.txt
-pip install Pillow
 
 # Set correct Django settings module 
-export DJANGO_SETTINGS_MODULE=myproject.settings
+export DJANGO_SETTINGS_MODULE=myproject.settings  # Change to your actual project name
 
 # Run Django commands
 python manage.py collectstatic --noinput
