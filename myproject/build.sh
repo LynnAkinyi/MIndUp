0e6b07d4347d5ac3246f452bbe1fb2df796eaec6
LynnAkinyi <lynnakinyi2003@gmail.com>
2025-05-01 00:57:29 +0300

    Remove Windows-specific dependency

--- myproject/build.sh
+++ myproject/build.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 set -e
 
+# Add project root to PYTHONPATH
+export PYTHONPATH=$PYTHONPATH:/opt/render/project/src/
+
 # Install dependencies
 pip install --upgrade pip
 pip install -r requirements.txt
 
-# Set Django settings
-export DJANGO_SETTINGS_MODULE=myproject.settings
+# Set correct Django settings module
+export DJANGO_SETTINGS_MODULE=myproject.myproject.settings
 
 # Run Django commands
 python manage.py collectstatic --noinput
